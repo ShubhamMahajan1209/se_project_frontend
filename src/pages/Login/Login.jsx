8e93d41db38def03d582340935351bd6655b614d
ShubhamMahajan1209 <123191420+ShubhamMahajan1209@users.noreply.github.com>
2025-04-17 02:28:23 +0530

    Update Login.jsx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -40,7 +40,7 @@ const Login = () => {
       dispatch(signInStart())
 
       const res = await axios.post(
-        "http://localhost:3000/api/auth/signin",
+        "https://se-project-backend-2.onrender.com/api/auth/signin",
         { email, password },
         { withCredentials: true }
       )
