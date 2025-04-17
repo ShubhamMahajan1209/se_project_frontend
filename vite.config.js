36ad14b2c163421259080fb455c67ac497bf3d3e
ShubhamMahajan1209 <123191420+ShubhamMahajan1209@users.noreply.github.com>
2025-04-17 11:46:24 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-});
-
+  server: {
+    host: '0.0.0.0', // Only needed for local dev
+    port: 5173,      // Optional
+  }
+})
